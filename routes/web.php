3fd9a1d14e7920cf90766f0d05fb8baa31545fad
lurrutia <luisebastian@live.cl>
2022-01-27 11:35:24 -0300

    sad

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\CentroController;
 */
 
 /* Route::get('/', function () {
-    return view('auth.login');
+  dasdsa  return view('auth.login');
 }); */
 
 Route::get('/', function () {
