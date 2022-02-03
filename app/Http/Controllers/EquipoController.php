75886dac317ffcb549832612ea631525da0303e0
lurrutia <luisebastian@live.cl>
2022-02-03 09:09:15 -0300

    Juevestamoscasi

--- app/Http/Controllers/EquipoController.php
+++ app/Http/Controllers/EquipoController.php
@@ -55,9 +55,9 @@ class EquipoController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+    {/*
         request()->validate(Equipo::$rules);
-
+ */
 
         /* $data=$request->all();
  */
@@ -75,22 +75,6 @@ class EquipoController extends Controller
         $clientes->save();
 
         $lastid=$clientes->id;   ///es aca
-
-
-
-        if(count($request->cod_equipo) > 0) ///es aca
-        {
-
-                $data2[]=array(
-                    'id_equipo'=>$lastid,
-                    'tipo_movimiento'=>$request->tipo_movimiento = "Compra",
-                    'fecha_movimiento'=>$request->fecha_ingreso[$movimiento],
-                    'tipo_documento'=>$request->tipo_documento[$movimiento],
-                    'n_documento'=>$request->n_documento[$movimiento],
-                );
-                Movimiento::insert($data2);
-          }
-            }
 
 
         return redirect()->route('equipos.index')
