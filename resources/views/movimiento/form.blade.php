62079c133835361cfff4479ad52dceff27a43e69
lurrutia <luisebastian@live.cl>
2022-02-03 15:48:15 -0300

    jueves03-48

---
+++ resources/views/movimiento/form.blade.php
@@ -0,0 +1,39 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('id_movimiento') }}
+            {{ Form::text('id_movimiento', $movimiento->id_movimiento, ['class' => 'form-control' . ($errors->has('id_movimiento') ? ' is-invalid' : ''), 'placeholder' => 'Id Movimiento']) }}
+            {!! $errors->first('id_movimiento', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('tipo_movimiento') }}
+            {{ Form::text('tipo_movimiento', $movimiento->tipo_movimiento, ['class' => 'form-control' . ($errors->has('tipo_movimiento') ? ' is-invalid' : ''), 'placeholder' => 'Tipo Movimiento']) }}
+            {!! $errors->first('tipo_movimiento', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('fecha_movimiento') }}
+            {{ Form::text('fecha_movimiento', $movimiento->fecha_movimiento, ['class' => 'form-control' . ($errors->has('fecha_movimiento') ? ' is-invalid' : ''), 'placeholder' => 'Fecha Movimiento']) }}
+            {!! $errors->first('fecha_movimiento', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('tipo_documento') }}
+            {{ Form::text('tipo_documento', $movimiento->tipo_documento, ['class' => 'form-control' . ($errors->has('tipo_documento') ? ' is-invalid' : ''), 'placeholder' => 'Tipo Documento']) }}
+            {!! $errors->first('tipo_documento', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('n_documento') }}
+            {{ Form::text('n_documento', $movimiento->n_documento, ['class' => 'form-control' . ($errors->has('n_documento') ? ' is-invalid' : ''), 'placeholder' => 'N Documento']) }}
+            {!! $errors->first('n_documento', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('id_equipo') }}
+            {{ Form::text('id_equipo', $movimiento->id_equipo, ['class' => 'form-control' . ($errors->has('id_equipo') ? ' is-invalid' : ''), 'placeholder' => 'Id Equipo']) }}
+            {!! $errors->first('id_equipo', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</div>
---
+++ resources/views/movimiento/show.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('template_title')
+    {{ $movimiento->name ?? 'Show Movimiento' }}
+@endsection
+
+@section('content')
+    <section class="content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="float-left">
+                            <span class="card-title">Show Movimiento</span>
+                        </div>
+                        <div class="float-right">
+                            <a class="btn btn-primary" href="{{ route('movimientos.index') }}"> Back</a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        
+                        <div class="form-group">
+                            <strong>Id Movimiento:</strong>
+                            {{ $movimiento->id_movimiento }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Tipo Movimiento:</strong>
+                            {{ $movimiento->tipo_movimiento }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Fecha Movimiento:</strong>
+                            {{ $movimiento->fecha_movimiento }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Tipo Documento:</strong>
+                            {{ $movimiento->tipo_documento }}
+                        </div>
+                        <div class="form-group">
+                            <strong>N Documento:</strong>
+                            {{ $movimiento->n_documento }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Id Equipo:</strong>
+                            {{ $movimiento->id_equipo }}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
